8774397459a34cea196a2e5925893846536b4d27
wahib <wahib.tech@gmail.com>
2014-10-27 23:29:22 +0100

    Basic and first implementation of Android project with 100 rows dataset running

--- gen/edu/sinica/citi/mac/android/actclassification/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.sinica.citi.mac.android.actclassification;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
